cd1147a18939e8a42fbf3c9ae4e377b0f0bc124a
Csehó Dániel <csehodani@gmail.com>
2023-07-03 21:39:55 +0200

    Remove the if-else statement

--- weather/src/main/java/com/example/weather/controllers/WeatherController.java
+++ weather/src/main/java/com/example/weather/controllers/WeatherController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequiredArgsConstructor
 public class WeatherController {
@@ -21,13 +23,11 @@ public class WeatherController {
                                                      @RequestParam(value = "lon", required = false) Double lon) {
         WeatherDataDto weatherData = searchService.search(location, lat, lon);
 
-        if (weatherData != null) {
-            weatherDataService.save(weatherData);
-
-            return ResponseEntity.ok(weatherData);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-
+        return Optional.ofNullable(weatherData)
+                .map(it -> {
+                    weatherDataService.save(it);
+                    return ResponseEntity.ok(it);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
